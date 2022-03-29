f4aa1207406eb1a30ead417b170efc0127cbce84
annanbeck <58147877+annanbeck@users.noreply.github.com>
2022-03-29 09:59:01 -0500

    Chapter 8

--- js/main.js
+++ js/main.js
@@ -1,75 +1,162 @@
-//initialize function called when the script loads
-function initialize(){
-    cities();
+//execute script when window is loaded
+window.onload = function(){
+
+    var w = 900, h = 500;
+
+    var container = d3.select("body") //get the <body> element from the DOM
+        .append("svg") //put a new svg in the body
+        .attr("width", w) //assign the width
+        .attr("height", h) //assign the height
+        .attr("class", "container") //always assign a class (as the block name) for styling and future selection
+        .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
+
+
+    //innerRect block
+    var innerRect = container.append("rect") //put a new rect in the svg
+        .datum(400)
+        .attr("width", function(d){ //rectangle width
+            return d * 2; //400 * 2 = 800
+        }) 
+        .attr("height", function(d){ //rectangle height
+            return d; //400
+        })
+        .attr("class", "innerRect") //class name
+        .attr("x", 50) //position from left on the x (horizontal) axis
+        .attr("y", 50) //position from top on the y (vertical) axis
+        .style("fill", "#FFFFFF"); //fill color
+
+        var cityPop = [
+            { 
+                city: 'Madison',
+                population: 233209
+            },
+            {
+                city: 'Milwaukee',
+                population: 594833
+            },
+            {
+                city: 'Green Bay',
+                population: 104057
+            },
+            {
+                city: 'Superior',
+                population: 27244
+            }
+        ];
+    
+    var x = d3.scaleLinear() //create the scale
+        .range([90, 750]) //output min and max
+        .domain([0, 3]); //input min and max
+
+        //find the minimum value of the array
+    var minPop = d3.min(cityPop, function(d){
+        return d.population;
+    });
+
+    //find the maximum value of the array
+    var maxPop = d3.max(cityPop, function(d){
+        return d.population;
+    });
+
+    //scale for circles center y coordinate
+    var y = d3.scaleLinear()
+        .range([450, 50]) //was 440, 95
+        .domain([0, 700000]); //was minPop, maxPop
+    
+    
+   var color = d3.scaleLinear()
+        .range([
+            "#FDBE85",
+            "#D94701"
+        ])
+        .domain([
+            minPop, 
+            maxPop
+        ]);
+
+    var circles = container.selectAll(".circles") //create an empty selection
+        .data(cityPop) //here we feed in an array
+        .enter() //one of the great mysteries of the universe
+        .append("circle") //inspect the HTML--holy crap, there's some circles there
+        .attr("class", "circles")
+        .attr("id", function(d){
+            return d.city;
+        })
+        .attr("r", function(d){
+            //calculate the radius based on population value as circle area
+            var area = d.population * 0.01;
+            return Math.sqrt(area/Math.PI);
+        })
+        .attr("cx", function(d, i){
+            //use the index to place each circle horizontally
+            return 90 + (i * 180);
+        })
+        .attr("cy", function(d){
+            //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
+            return 450 - (d.population * 0.0005);
+        })
+        .attr("cx", function(d, i){
+            //use the scale generator with the index to place each circle horizontally
+            return x(i);
+        })
+        //Example 2.8 line 38
+        .attr("cy", function(d){
+            return y(d.population);
+        })
+        .style("fill", function(d, i){ //add a fill based on the color scale generator
+            return color(d.population);
+        })
+        .style("stroke", "#000") //black circle stroke
+        //add axis
+        var yAxis = d3.axisLeft(y);
+
+        //create axis g element and add axis
+        var axis = container.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(50, 0)")
+                .call(yAxis)
+        
+        //below Example 3.9...create a text element and add the title
+        var title = container.append("text")
+            .attr("class", "title")
+            .attr("text-anchor", "middle")
+            .attr("x", 450)
+            .attr("y", 30)
+            .text("City Populations")
+        
+        var labels = container.selectAll(".labels")
+            .data(cityPop)
+            .enter()
+            .append("text")
+            .attr("class", "labels")
+            .attr("text-anchor", "left")
+            .attr("y", function(d){
+                //vertical position centered on each circle
+                return y(d.population);
+            })
+
+            //first line of label
+        var nameLine = labels.append("tspan")
+            .attr("class", "nameLine")
+            .attr("x", function(d,i){
+        //horizontal position to the right of each circle
+                 return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+            })
+            .text(function(d){
+                return d.city;
+            })
+
+//create format generator
+var format = d3.format(",");
+
+//Example 3.16 line 1...second line of label
+var popLine = labels.append("tspan")
+    .attr("class", "popLine")
+    .attr("x", function(d,i){
+        return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+    })
+    .attr("dy", "15") //vertical offset
+    .text(function(d){
+        return "Pop. " + format(d.population); //use format generator to format numbers
+    });
 };
-
-//function to create a table with cities and their populations
-function cities(){
-    //define two arrays for cities and population
-    var cities = [
-        'Lincoln',
-        'Madison',
-        'Raleigh',
-        'Portland'
-    ];
-    var population = [
-        1,
-        2,
-        3,
-        4
-    ];
-
-    //create the table element
-    var table = document.createElement("table");
-
-    //create a header row
-    var headerRow = document.createElement("tr");
-
-    //add the "City" column
-    var cityHeader = document.createElement("th");
-    cityHeader.innerHTML = "City";
-    headerRow.appendChild(cityHeader);
-
-    //add the "Population" column
-    var popHeader = document.createElement("th");
-    popHeader.innerHTML = "Happiness";
-    headerRow.appendChild(popHeader);
-
-    //add the row to the table
-    table.appendChild(headerRow);
-
-    //loop to add a new row for each city
-    for (var i = 0; i < cities.length; i++){
-        var tr = document.createElement("tr");
-
-        var city = document.createElement("td");
-        if (cities[i] == 'Madison'){
-            city.innerHTML = 'My City';
-        } else {
-            city.innerHTML = cities[i];
-        }
-
-        tr.appendChild(city);
-
-        var pop = document.createElement("td");
-        if (population[i] === 2){
-            pop.innerHTML = 'Happiest';
-        } else if (population[i] === 1){
-            pop.innerHTML = 'Overrated'
-        } else {
-            pop.innerHTML = population[i];
-        };
-
-        tr.appendChild(pop);
-
-        table.appendChild(tr);
-    };
-
-
-    //add the table to the div in index.html
-    var mydiv = document.querySelector("#mydiv");
-    mydiv.appendChild(table);
-};
-
-//call the initialize function when the window has loaded
-window.onload = initialize();
