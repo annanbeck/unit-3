36fa98fae9f2cd10be7a6350195c912641bc678e
annanbeck <58147877+annanbeck@users.noreply.github.com>
2022-04-06 09:06:52 -0500

    chapter 9

--- js/main.js
+++ js/main.js
@@ -1,162 +1,74 @@
-//execute script when window is loaded
-window.onload = function(){
+//begin script when window loads
+window.onload = setMap();
 
-    var w = 900, h = 500;
+//set up choropleth map
+function setMap(){
+    //map frame dimensions
+    var width = 330,
+        height = 460;
 
-    var container = d3.select("body") //get the <body> element from the DOM
-        .append("svg") //put a new svg in the body
-        .attr("width", w) //assign the width
-        .attr("height", h) //assign the height
-        .attr("class", "container") //always assign a class (as the block name) for styling and future selection
-        .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
+    //create new svg container for the map
+    var map = d3
+        .select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    //create Albers equal area conic projection centered on France
+    var projection = d3
+        .geoConicEqualArea()
+        .center([0, 44.5])
+        .rotate([90, 0, 0])
+        .parallels([40, 50])
+        .scale(4000)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geoPath().projection(projection);
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [d3.csv("data/WI_ENVI.csv"),                    
+                    d3.json("data/wi_counties.topojson"),
+                    d3.json("data/wi_lakes.topojson"),                    
+                    d3.json("data/wi_states.topojson"),                   
+                    ];    
+    Promise.all(promises).then(callback);
 
 
-    //innerRect block
-    var innerRect = container.append("rect") //put a new rect in the svg
-        .datum(400)
-        .attr("width", function(d){ //rectangle width
-            return d * 2; //400 * 2 = 800
-        }) 
-        .attr("height", function(d){ //rectangle height
-            return d; //400
-        })
-        .attr("class", "innerRect") //class name
-        .attr("x", 50) //position from left on the x (horizontal) axis
-        .attr("y", 50) //position from top on the y (vertical) axis
-        .style("fill", "#FFFFFF"); //fill color
+    function callback(data) {
+        var csvData = data[0],
+            counties = data[1],
+            lakes = data[2],
+            states = data[3];
 
-        var cityPop = [
-            { 
-                city: 'Madison',
-                population: 233209
-            },
-            {
-                city: 'Milwaukee',
-                population: 594833
-            },
-            {
-                city: 'Green Bay',
-                population: 104057
-            },
-            {
-                city: 'Superior',
-                population: 27244
-            }
-        ];
-    
-    var x = d3.scaleLinear() //create the scale
-        .range([90, 750]) //output min and max
-        .domain([0, 3]); //input min and max
+    //translate europe TopoJSON
+    var wi_counties = topojson.feature(counties, counties.objects.wi_counties).features,
+        wi_lakes = topojson.feature(lakes, lakes.objects.wi_lakes),
+        wi_states = topojson.feature(states, states.objects.wi_states);
 
-        //find the minimum value of the array
-    var minPop = d3.min(cityPop, function(d){
-        return d.population;
-    });
+    //examine the results
+    console.log(wi_counties);
+    console.log(wi_states);
+    console.log(wi_lakes);
 
-    //find the maximum value of the array
-    var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
+    //add Europe countries to map
+/*var states = map.append("path")
+    .datum(wi_states)
+    .attr("class", "name")
+    .attr("d", path);
 
-    //scale for circles center y coordinate
-    var y = d3.scaleLinear()
-        .range([450, 50]) //was 440, 95
-        .domain([0, 700000]); //was minPop, maxPop
-    
-    
-   var color = d3.scaleLinear()
-        .range([
-            "#FDBE85",
-            "#D94701"
-        ])
-        .domain([
-            minPop, 
-            maxPop
-        ]);
+var lakes = map.append("path")
+    .datum(wi_lakes)
+    .attr("class", "name")
+    .attr("d", path);*/
 
-    var circles = container.selectAll(".circles") //create an empty selection
-        .data(cityPop) //here we feed in an array
-        .enter() //one of the great mysteries of the universe
-        .append("circle") //inspect the HTML--holy crap, there's some circles there
-        .attr("class", "circles")
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d){
-            //calculate the radius based on population value as circle area
-            var area = d.population * 0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx", function(d, i){
-            //use the index to place each circle horizontally
-            return 90 + (i * 180);
-        })
-        .attr("cy", function(d){
-            //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
-            return 450 - (d.population * 0.0005);
-        })
-        .attr("cx", function(d, i){
-            //use the scale generator with the index to place each circle horizontally
-            return x(i);
-        })
-        //Example 2.8 line 38
-        .attr("cy", function(d){
-            return y(d.population);
-        })
-        .style("fill", function(d, i){ //add a fill based on the color scale generator
-            return color(d.population);
-        })
-        .style("stroke", "#000") //black circle stroke
-        //add axis
-        var yAxis = d3.axisLeft(y);
-
-        //create axis g element and add axis
-        var axis = container.append("g")
-                .attr("class", "axis")
-                .attr("transform", "translate(50, 0)")
-                .call(yAxis)
-        
-        //below Example 3.9...create a text element and add the title
-        var title = container.append("text")
-            .attr("class", "title")
-            .attr("text-anchor", "middle")
-            .attr("x", 450)
-            .attr("y", 30)
-            .text("City Populations")
-        
-        var labels = container.selectAll(".labels")
-            .data(cityPop)
-            .enter()
-            .append("text")
-            .attr("class", "labels")
-            .attr("text-anchor", "left")
-            .attr("y", function(d){
-                //vertical position centered on each circle
-                return y(d.population);
-            })
-
-            //first line of label
-        var nameLine = labels.append("tspan")
-            .attr("class", "nameLine")
-            .attr("x", function(d,i){
-        //horizontal position to the right of each circle
-                 return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-            })
-            .text(function(d){
-                return d.city;
-            })
-
-//create format generator
-var format = d3.format(",");
-
-//Example 3.16 line 1...second line of label
-var popLine = labels.append("tspan")
-    .attr("class", "popLine")
-    .attr("x", function(d,i){
-        return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+//add France regions to map
+var counties = map.selectAll(".regions")
+    .data(wi_counties)
+    .enter()
+    .append("path")
+    .attr("class", function(d){
+        return "regions " + d.properties.name;
     })
-    .attr("dy", "15") //vertical offset
-    .text(function(d){
-        return "Pop. " + format(d.population); //use format generator to format numbers
-    });
+    .attr("d", path);
 };
+}
